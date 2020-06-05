d1d75a36bba0cbaaaade2722a27bea5ecd69c41b
NIR <nirschipper4@gmail.com>
2020-06-05 14:01:51 +0300

    Removed 'exclude = False' from get_peaks in line_detection.py. Changed the drawing of lines to be focused on the relevant part of the image (as opposed to the entire padded image).

--- line_detection.py
+++ line_detection.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from skimage.transform import radon, iradon
 from skimage.feature import peak_local_max
 import numpy as np
 
@@ -11,9 +10,9 @@ def get_intersection_points_of_line_and_image_boundary(peak, image, angles):
     slope = np.tan(np.deg2rad(peak_theta - 90))
     point_j = center_j + peak_r * np.cos(np.deg2rad(peak_theta))
     point_i = center_i - peak_r * np.sin(np.deg2rad(peak_theta))
-    i_s = np.arange(image.shape[1])
+    i_s = np.arange(image.shape[1]//2)+image.shape[1]//4
     j_s = point_i + (i_s - point_j) * slope * (-1)  # the y axis is inverted when using i,j image coordinates
-    in_image = (j_s > 0) & (j_s < image.shape[0])
+    in_image = (j_s > image.shape[0]//2) & (j_s < image.shape[0])
     point1 = (j_s[in_image][0], i_s[in_image][0])
     point2 = (j_s[in_image][-1], i_s[in_image][-1])
     if np.sum(in_image) <= 1:
@@ -21,18 +20,27 @@ def get_intersection_points_of_line_and_image_boundary(peak, image, angles):
         point2 = (image.shape[0] - 1, point_j)
     return point1, point2
 
-
-def get_peaks(image, num_peaks, neighborhood_size=1):
-    return peak_local_max(image, min_distance=neighborhood_size, num_peaks=num_peaks, exclude_border=False)
-
-
+def get_bias_and_slope_by_point_and_slope(peak, image, angles):
+    peak_r = peak[0]
+    peak_theta = angles[peak[1]]
+    slope = np.tan(np.deg2rad(peak_theta - 90))
+    center_i, center_j = image.shape[0] // 2, image.shape[1] // 2
+    shift_on_center_line = peak_r / np.cos(np.deg2rad(peak_theta))
+    point = (center_i, shift_on_center_line + center_j)
+    bias = point[0] + point[1] * slope
+    return bias, slope
+#
+def get_peaks(image, num_peaks, neighborhood_size=5):
+    return peak_local_max(image, min_distance=neighborhood_size, num_peaks=num_peaks)
+#
+#
 def get_peaks_in_window(image, r_delta, angles, num_peaks):
     middle = image.shape[0] // 2
     peaks = get_peaks(image[middle - r_delta: middle + r_delta, angles], num_peaks)
     for peak in range(num_peaks):
         peaks[peak] = [peaks[peak][0] - r_delta, angles[peaks[peak][1]]]
     return peaks
-
+#
 
 def get_horizontal_peaks(image, num_peaks):
     almost_horizontal = np.arange(75, 105)
@@ -48,5 +56,7 @@ def draw_lines(peaks, image, angles=np.arange(180)):
     for peak in peaks:
         point1, point2 = get_intersection_points_of_line_and_image_boundary(peak, image, angles)
         plt.plot([point1[1], point2[1]], [point1[0], point2[0]])
+
+
 
 
